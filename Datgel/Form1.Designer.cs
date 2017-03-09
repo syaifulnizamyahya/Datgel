6a24b59e5e7e23540eb8d3b5950ff34e7bedc119
Syaiful Nizam Yahya <syaifulnizamyahya@gmail.com>
2017-03-09 23:59:26 +0800

    The form should have two text boxes, one for the x coordinate and the other for the y coordinate. The text boxes should have accompanying labels to distinguish one from the other.
    The form should have a button labelled “Close”. When clicked, it should close the form and the application.

--- Datgel/Form1.Designer.cs
+++ Datgel/Form1.Designer.cs
@@ -29,6 +29,11 @@
 		private void InitializeComponent()
 		{
 			this.pointsGridView = new System.Windows.Forms.DataGridView();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.pointsGridView)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -44,22 +49,76 @@
 			this.pointsGridView.Size = new System.Drawing.Size(335, 393);
 			this.pointsGridView.TabIndex = 0;
 			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(32, 411);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 22);
+			this.textBox1.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 414);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(14, 17);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "x";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(170, 414);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(15, 17);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "y";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(191, 411);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 22);
+			this.textBox2.TabIndex = 3;
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button1.Location = new System.Drawing.Point(504, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 5;
+			this.button1.Text = "Close";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(591, 586);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
 			this.Controls.Add(this.pointsGridView);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			((System.ComponentModel.ISupportInitialize)(this.pointsGridView)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
 
 		private System.Windows.Forms.DataGridView pointsGridView;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button button1;
 	}
 }
 
--- Datgel/Form1.cs
+++ Datgel/Form1.cs
@@ -35,5 +35,9 @@ namespace Datgel
 			pointsGridView.DataSource = source;
 		}
 
+		private void button1_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
 	}
 }
